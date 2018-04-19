9fea16016b419efd0e3efefebd9917f8a89134af
Limtrack <limtrack@gmail.com>
2018-04-19 18:24:07 +0200

    Modificanto textos

--- models/phone.js
+++ models/phone.js
@@ -11,17 +11,15 @@ class PhoneModel {
         name: 'iPhone X',
         description: 'Maecenas erat justo, suscipit sit amet purus mollis, egestas lacinia eros. Vestibulum pellentesque felis sed felis porttitor pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed mollis nisl ut turpis cursus laoreet. Sed nulla massa, vulputate id elit et, rutrum sagittis orci. Nunc in accumsan massa.',
         price: 900,
-        color: 'black',
-        size: '14.36x7.09x0.77'
+        color: 'black'
       },
       {
         id: 2,
         image: 'https://www.t-mobile.com/content/dam/t-mobile/en-p/cell-phones/samsung/samsung-galaxy-s9-plus/lilac-purple/Samsung-Galaxy-S9-Plus-Lilac-Purple-2-3x.jpg',
         name: 'Samsung Galaxy S9',
-        description: 'Maecenas erat justo, suscipit sit amet purus mollis, egestas lacinia eros. Vestibulum pellentesque felis sed felis porttitor pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed mollis nisl ut turpis cursus laoreet. Sed nulla massa, vulputate id elit et, rutrum sagittis orci. Nunc in accumsan massa.',
+        description: 'Donec vitae quam sed augue rutrum euismod. Curabitur eget dolor quis dolor elementum tristique ut ac odio. Mauris eros dolor, finibus vitae justo placerat, faucibus tincidunt nunc. Sed dolor nisl, rhoncus sit amet gravida a, sodales in lorem.',
         price: 800,
-        color: 'blue',
-        size: '15.20x7.34x0.90'
+        color: 'blue'
       }
     ]
   }
